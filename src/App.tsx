6dab2108bc4567d0da5c1f6a7dde5fb04d57542b
plastic041 <plastic041@gmail.com>
2020-09-23 23:06:14 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TemplateImage from "./Resources/Images/template.png";
+import Draggable from "react-draggable";
+import "./App.css";
 
 function App() {
+  const [file, setFile] = useState<string>("");
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("loaded1");
+    if (e.target.files != null) {
+      let reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload = () => {
+        console.log("loaded2");
+        if (reader.result != null) {
+          console.log("loaded3");
+          setFile(reader.result as string);
+        }
+      };
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={TemplateImage} alt="Template"></img>
+      <input className="text mainText" placeholder="아이템 이름 작성" />
+      <textarea
+        className="text discriptionText"
+        placeholder="아이템 설명 작성"
+      />
+      <Draggable>
+        <img className="itemImage" src={file} alt="" />
+      </Draggable>
+      <div>
+        <span>이미지 넣기 ➡ </span>
+        <input type="file" name="file" onChange={(e) => handleFileChange(e)} />
+      </div>
     </div>
   );
 }
